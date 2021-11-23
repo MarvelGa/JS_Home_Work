a827247fbcd028efa24548287f6d023f6eb40a29
dxdNvqKP <7VDO8j7kEg>
2021-11-23 17:02:08 +0200

    added homework1

---
+++ HOMEWORK1/task6.js
@@ -0,0 +1,36 @@
+let firstName;
+let lastName;
+let numberOfGroup;
+let yearOfBirth;
+
+firstName = 'Halyna';
+lastName = 'Tesliuk';
+numberOfGroup = 654;
+yearOfBirth = 1987;
+let isMarried = true;
+
+const data = new Array(firstName, lastName, numberOfGroup, yearOfBirth, isMarried);
+let numberType = '';
+let booleanType = '';
+let stringType = '';
+for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === 'number') {
+        numberType += data[i] + ' ';
+    }
+    if (typeof data[i] === 'boolean') {
+        booleanType += data[i] + ' ';
+    }
+    if (typeof data[i] === 'string') {
+        stringType += data[i] + ' ';
+    }
+}
+
+document.write('Value variables with number type : ' + numberType + '<br /\>' + '<br /\>');
+document.write('Value of variables with boolean type : ' + booleanType + '<br\/>' + '<br /\>');
+document.write('Value of variables with string type : ' + stringType + '<br\/>' + '<br /\>');
+
+let carBrand = null;
+let laptopBrand;
+
+document.write('Type of carBrand: ' + typeof carBrand + '<br\/>' + '<br\/>');
+document.write('Type of laptopBrand: ' + typeof laptopBrand + '<br\/>')
